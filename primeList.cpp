fe68d1eebe4b7ac3c0c435369cb1009001a77fe3
Michael B <1012481+michaelbraunstein@users.noreply.github.com>
2019-01-18 10:23:58 -0500

    Add files via upload

--- primeList.cpp
+++ primeList.cpp
@@ -10,44 +10,29 @@ Create a list of prime numbers
 #include<cmath>
 using namespace std;
 int number, loop;
-bool isPrime;
+	bool isPrime;
 
 int main()
 {
+	
 	cout << "List of Prime Numbers" << endl;
-	for (number = 1; number <= 14; number++) {
+	for (number = 2; number <= 999140000; number++) {
 		 
-		for (loop = 1; loop < number; loop++) {
-			//cout << number << " modulus by " << loop << " is: " << (number % loop) << endl;
+		for (loop = 2; loop*loop < number; loop++) {
+
 
 
 			if (number % loop == 0) {
-				isPrime = true;
-				cout << number << " is  a  prime number. " << endl;
+				break;
 			}
+			//isPrime = false;
+			else if (loop + 1 > sqrt(number)) {
+				cout << "this " << number << " is a prime numer." << endl;
+			} //else
 
-			   
-
-
-
-			}
-
-		}
-	}
-
-
-
-	
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+		}// loop
+ 
+			} //number
+				} //main
+			
+				  
